345ba8a3fa731b5781c0333dc0045e31ef6c7066
Greeniac916 <greeniac916@gmail.com>
2018-03-10 16:30:44 -0500

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,13 +1,30 @@
 const styles = ["Default", "James_People"];
+let styleIndex = 0;
 
 function setNextStyle() {
+  styleIndex = (styleIndex + 1) % styles.length;
+  changeCSS("style/" + styles[styleIndex] + "/style.css");
   console.log("Next");
 }
 
 function setLastStyle() {
   console.log("Last");
+  styleIndex = (styleIndex - 1) % styles.length;
+  changeCSS("style/" + styles[styleIndex] + "/style.css");
+  console.log("Next");
 }
 
 function toggleText() {
   console.log("Text");
+}
+
+function changeCSS(cssFile) {
+    var oldlink = document.getElementsByTagName("link").item(1);
+  
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+  
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
