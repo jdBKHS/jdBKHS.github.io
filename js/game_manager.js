b055e3500fda7522f8cb073d5dbff2c1b5addd56
Greeniac916 <greeniac916@gmail.com>
2018-03-10 18:07:20 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,5 +1,3 @@
-let styleIndex;
-
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
@@ -36,8 +34,6 @@ GameManager.prototype.isGameTerminated = function () {
 // Set up the game
 GameManager.prototype.setup = function () {
   var previousState = this.storageManager.getGameState();
-  
-  styleIndex = this.storageManager.getGameStyle() || 0;
   
   // Reload the game from a previous game if present
   if (previousState) {
@@ -84,8 +80,6 @@ GameManager.prototype.actuate = function () {
   if (this.storageManager.getBestScore() < this.score) {
     this.storageManager.setBestScore(this.score);
   }
-  
-  this.storageManager.setGameStyle(styleIndex);
 
   // Clear the state when the game is over (game over only, not win)
   if (this.over) {
